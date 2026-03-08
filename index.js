d9fa85bec0d72826c7740aaa624b651d48b8a479
SBR <69163271+Shivalkyle28@users.noreply.github.com>
2026-03-08 12:57:49 -0400

    Add FizzBuzz implementation to index.js

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ console.log(typeof(age))
 console.log(`Hello my name is ${name}, I'm ${age} years old`);
 
 console.log(`I was born in ${2020 - age}`);
+
+for(let i = 1; i <= 50; i++){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("fizzbuzz");
+  }
+  else if(i % 3 === 0){
+    console.log("fizz");
+  }
+  else if(i % 5 === 0){
+    console.log("buzz");
+  }
+  else{
+    console.log(i);
+  }
+}
