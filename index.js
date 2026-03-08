0c743ad3fcabcc693e5d4c8c6e29e6b4eb53359a
SBR <69163271+Shivalkyle28@users.noreply.github.com>
2026-03-08 12:56:13 -0400

    Introduce name and age variables with console logs
    Added variables for name and age, and logged details.

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ const pi = 3.14;
 let area = radius * radius * pi;
 
 console.log(area);
+
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))
+console.log(typeof(age))
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+
+console.log(`I was born in ${2020 - age}`);
